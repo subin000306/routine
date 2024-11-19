443149d20c7fde289af22e8f57dddc581bd95268
Shinmilli <shinmilli100@gmail.com>
2024-11-20 00:59:56 +0900

    commit

--- client/client/src/styles/colors.js
+++
@@ -1,5 +0,0 @@
-export const primaryColor = "#EF948C";
-export const secondaryColor = "#F2E6D7";
-export const tertiaryColor = "#9198EB";
-export const buttontextColor = "#FFFFFF";
-export const textColor = "#000000";

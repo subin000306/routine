d3d261a2edd1c01e013c8c4e86f177727cdec6d1
Shinmilli <shinmilli100@gmail.com>
2024-12-02 20:02:16 +0900

    타입테스트 로직 수정2

--- client/src/pages/TypeTest/index.js
+++ client/src/pages/TypeTest/index.js
@@ -12,54 +12,58 @@ function TypeTest() {
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
   const handleAnswer = (typeName, score) => {
-      const newScores = { ...scores };
-      if (!newScores[typeName]) {
-          newScores[typeName] = [];
-      }
-      newScores[typeName].push(score); // Save the score for the answer
-      setScores(newScores);
+    const newScores = { ...scores };
+    if (!newScores[typeName]) {
+      newScores[typeName] = [];
+    }
+    newScores[typeName].push(score); // Save the score for the answer
+    setScores(newScores);
 
-      // Move to the next question or type
-      const nextQuestionIndex = currentQuestionIndex + 1;
-      if (nextQuestionIndex < userTypeData.types[currentTypeIndex].questions.length) {
-          setCurrentQuestionIndex(nextQuestionIndex);
+    // Move to the next question or type
+    const nextQuestionIndex = currentQuestionIndex + 1;
+    if (nextQuestionIndex < userTypeData.types[currentTypeIndex].questions.length) {
+      setCurrentQuestionIndex(nextQuestionIndex);
+    } else {
+      const nextTypeIndex = currentTypeIndex + 1;
+      if (nextTypeIndex < userTypeData.types.length) {
+        setCurrentTypeIndex(nextTypeIndex);
+        setCurrentQuestionIndex(0);
       } else {
-          const nextTypeIndex = currentTypeIndex + 1;
-          if (nextTypeIndex < userTypeData.types.length) {
-              setCurrentTypeIndex(nextTypeIndex);
-              setCurrentQuestionIndex(0);
-          } else {
-              // Save results and navigate to result page
-              localStorage.setItem('personalityTestResults', JSON.stringify(newScores));
-              navigate('/typetest/result');
-          }
+        // Send results to the server and navigate to result page
+        axios
+          .post("http://localhost:3000/api/Usertype", { scores: newScores })
+          .then(() => {
+            navigate("/typetest/result");
+          })
+          .catch((error) => console.error("Error sending results:", error));
       }
+    }
   };
 
   const currentType = userTypeData.types[currentTypeIndex];
   const currentQuestion = currentType.questions[currentQuestionIndex];
 
   const renderScoreButtons = () => {
-      const scores = [1, 2, 3, 4, 5]; // Scores from 1 to 5
-      return scores.map(score => (
-          <StyledButton key={score} onClick={() => handleAnswer(currentType.name, score)}>
-              {score}
-          </StyledButton>
-      ));
+    const scores = [1, 2, 3, 4, 5]; // Scores from 1 to 5
+    return scores.map((score) => (
+      <StyledButton key={score} onClick={() => handleAnswer(currentType.name, score)}>
+        {score}
+      </StyledButton>
+    ));
   };
 
   return (
-      <Wrap>
-          <Wrap_container>
-              <h1>Personality Test</h1>
-              {currentQuestion && (
-                  <div>
-                      <p>{currentQuestion.a}</p>
-                      {renderScoreButtons()}
-                  </div>
-              )}
-          </Wrap_container>
-      </Wrap>
+    <Wrap>
+      <Wrap_container>
+        <h1>Personality Test</h1>
+        {currentQuestion && (
+          <div>
+            <p>{currentQuestion.a}</p>
+            {renderScoreButtons()}
+          </div>
+        )}
+      </Wrap_container>
+    </Wrap>
   );
 }
 
